2e4b4e60339e3e6834fdd39b225034c1331ba5b0
Archana <archanak233226@gmail.com>
2023-04-14 23:06:39 +0530

    first commit

---
+++ JavaScript/Function.js
@@ -0,0 +1,33 @@
+function funct(name,morning,working){
+    console.log("hello "+ name + " Good Morning");
+    console.log(" how are you?" + morning)
+    console.log(" what are you doing?" + name);
+    console.log(name + " is a good person");
+   
+}
+let a = "archana";
+let b = "mithun";
+let c = "amit";
+let d = "anuj";
+let e = "achal";
+let f = "kartik";
+let m = "archana";
+let n = "mithun";
+let o = "amit";
+let p = "anuj";
+let q = "achal";
+let r = "kartik";
+let working;
+funct(a,m,working);
+funct(b,n,working);
+funct(c,o,working);
+funct(d,p,working);
+funct(e,q,working);
+funct(f,r,working);
+// console.log(a + " is a good person");
+// console.log(b + " is a good person");
+// console.log(c + " is a good person");
+// console.log(d + " is a good person");
+// console.log(e + " is a good person");
+// console.log(f + " is a good person");
+
